611a40ca9d95cd4c2ba7c80d95f2303ffbf68fd8
Linus Arver <linusarver@gmail.com>
2014-10-15 03:47:05 -0700

    A2: make prettier listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+  description:
+    %{<p>
+      Ruby is faster than 10 fast languages! It is used only by humans, not
+      plants or animals.
+      </p>},
+    image_url: 'ruby.jpg',
+    price: 49.95)
+
+Product.create!(title: 'Haskell',
+  description:
+    %{<p>
+      <em>Haskell</em> is cooler than the 10 most uncool languages! It is used
+      by lots of humans.
+    </p>},
+  image_url: 'rtp.jpg',
+  price: 34.95)
