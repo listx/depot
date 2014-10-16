461b56c18f01541197da1c8b53cdc91ff1eacacc
Linus Arver <linusarver@gmail.com>
2014-10-15 20:22:01 -0700

    C5: caching of partial results

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,4 +15,8 @@ class Product < ActiveRecord::Base
   }
   # FIXME: testing against 0.01 (1 cent) allows bogus value `3.001'
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
