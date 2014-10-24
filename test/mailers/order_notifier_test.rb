5184b2b0e7edda4638d50dcff116940315625573
Linus Arver <linusarver@gmail.com>
2014-10-24 05:37:32 -0700

    H1: send confirmation email: html email and testing
    The two tests in `order_notifier_test.rb` fail; the book does not
    explain how the orders are generated. Presumably, we can fix this later.
    (NOTE: The actual webapp does generate a proper email; it's only that
    the test environment does not generate orders correctly).

--- test/mailers/order_notifier_test.rb
+++ test/mailers/order_notifier_test.rb
@@ -2,19 +2,25 @@ require 'test_helper'
 
 class OrderNotifierTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderNotifier.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderNotifier.received(orders(:one))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    # FIXME: this assertion fails, because the ordered items are never generated
+    # in the test. (I.e., under "You ordered the following items:" list, there
+    # should be a list of items, but this list is empty.)
+    assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderNotifier.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderNotifier.shipped(orders(:one))
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    # FIXME: Likewise, this assertion also fails because of the empty ordered
+    # items list.
+    assert_match /<td>1&times;<\/td>\s*<td>Programming Ruby 1.9<\/td>/,
+      mail.body.encoded
   end
 
 end
