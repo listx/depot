7fe51609c6ed466c4ab108223184d2bed257ba22
Linus Arver <linusarver@gmail.com>
2014-10-27 05:39:22 -0700

    order_notifier_test: fix fixtures for mailers test
    The changes to the test fixtures are needed to make the mailer tests
    pass as-is under test/mailers/order_notifier_test.rb; previously, the
    assert_match regexes would fail because of blank products (line items).
    Now, the line items are populated as expected.
    
    Historically, the file `line_items.yml` was created as part of the
    `rails generate scaffold LineItem product:references cart:belong_to`
    command back in bbe46dc7fd28a00da220ea0a3db9b52c216c89b4.
    
    In 5d8103ca88efd9192807c9db9991127cf40b48e5, we added the values to this
    file (the value fields after the colons were blank).
    
    It appears that in "Iteration G1: Capturing an Order" in the book (p.
    167) there is a link to a "fixed" `line_items.yml`. Obviously, we had
    overlooked this file. Interestingly, however, that section is the book
    is the only time that `line_items.yml` is referenced as a source link,
    and its contents are thus:
    
        # Read about fixtures at
        # http://api.rubyonrails.org/classes/ActiveRecord/Fixtures.html
    
        one:
          product: ruby
        #START_HIGHLIGHT
          order: one
        #END_HIGHLIGHT
    
        two:
          product: ruby
          cart: one
    
    But to make the mailer tests pass, we only had to change some of the
    lines in `line_items.yml`. The "<model>_id" syntax was probably chosen
    by a more recent version of Rails, rather than the "<model>" syntax used
    in the book (e.g., "product_id" instead of "product").

--- test/mailers/order_notifier_test.rb
+++ test/mailers/order_notifier_test.rb
@@ -6,9 +6,6 @@ class OrderNotifierTest < ActionMailer::TestCase
     assert_equal "Pragmatic Store Order Confirmation", mail.subject
     assert_equal ["dave@example.org"], mail.to
     assert_equal ["depot@example.com"], mail.from
-    # FIXME: this assertion fails, because the ordered items are never generated
-    # in the test. (I.e., under "You ordered the following items:" list, there
-    # should be a list of items, but this list is empty.)
     assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
   end
 
@@ -17,8 +14,6 @@ class OrderNotifierTest < ActionMailer::TestCase
     assert_equal "Pragmatic Store Order Shipped", mail.subject
     assert_equal ["dave@example.org"], mail.to
     assert_equal ["depot@example.com"], mail.from
-    # FIXME: Likewise, this assertion also fails because of the empty ordered
-    # items list.
     assert_match /<td>1&times;<\/td>\s*<td>Programming Ruby 1.9<\/td>/,
       mail.body.encoded
   end
